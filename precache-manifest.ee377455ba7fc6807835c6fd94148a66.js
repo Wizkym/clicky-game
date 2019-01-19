a85937176afb831f7d462a1599f3d1576cfdf47a
Kelvin Jones <kevinjones4453@gmail.com>
2019-01-18 20:51:50 -0500

    Updates

---
+++ precache-manifest.ee377455ba7fc6807835c6fd94148a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa243e1bdce267df5b7",
+    "url": "/clicky-game/static/css/main.8bb96b0c.chunk.css"
+  },
+  {
+    "revision": "3aa243e1bdce267df5b7",
+    "url": "/clicky-game/static/js/main.3aa243e1.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/clicky-game/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "6eb80fd8725a1ef1ef907b9c8e192836",
+    "url": "/clicky-game/index.html"
+  }
+];
